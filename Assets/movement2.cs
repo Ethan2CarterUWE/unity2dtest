2f1d78bcd0b935aa00a6bab7b3d8afc0476ce02b
Ethan <Ethan2.Carter@live.uwe.ac.uk>
2020-10-25 01:51:21 +0100

    PRe Delete

--- Assets/movement2.cs
+++ Assets/movement2.cs
@@ -5,67 +5,99 @@ using UnityEngine;
 public class movement2 : MonoBehaviour
 {
 
-    public float turnpower = 2;
-    private Vector3 myVel;
+    public float turnSpeed = 7;
+    //private Vector3 vel;
     private Vector3 acceleration;
-    private float maxSpeed = 0.05f;
+    public float maxSpeed = 0.05f;
+    public float maxReverse = 0.01f;
 
     void Start()
     {
-        myVel = new Vector3(0, 0, 0);
+        //vel = new Vector3(0, 0, 0);
         acceleration = new Vector3(0, 0, 0);
 
     }
 
     void FixedUpdate()
-    {
+    {       
         
-
-
-        if (Input.GetKey(KeyCode.W))
+        /*if (Input.GetKey(KeyCode.W))
         {
-            //float currentspeed = myVel.magnitude;
-           // myVel.Normalize();
+            float currentspeed = vel.magnitude;
+            vel.Normalize();
             transform.Translate(0, acceleration.y, 0);
             if (acceleration.y <=0.30)
             {
                 
-               // if (myVel.magnitude <= maxSpeed)
+                if (vel.magnitude <= maxSpeed)
                 {
                     acceleration.y += 0.001f;
                 }
             }
           
+        }*/
+
+        if (Input.GetKey(KeyCode.W))
+        {
+           
+            transform.Translate(0, acceleration.y, 0);
+            if (acceleration.y <= maxSpeed)
+            {
+
+                if (acceleration.y <= maxSpeed)
+                {
+                    acceleration.y += 0.001f;
+                }
+            }
+
         }
+
+        /*if (Input.GetKey(KeyCode.Space))
+        {
+            float currentspeed = vel.magnitude;
+            vel.Normalize();
+            transform.Translate(0, acceleration.y, 0);
+
+            if (acceleration.y >= maxReverse)
+            {
+                if (vel.magnitude >= -2 * maxSpeed)
+                {
+                    acceleration.y -= 0.005f;
+                }
+            }           
+        }*/
 
         if (Input.GetKey(KeyCode.Space))
         {
-            float currentspeed = myVel.magnitude;
-            myVel.Normalize();
             transform.Translate(0, acceleration.y, 0);
 
-            if (myVel.magnitude >= -2 *maxSpeed)
+            if (acceleration.y >= maxReverse)
             {
-                acceleration.y -= 0.005f;
+                if (acceleration.y >= -2 * maxSpeed)
+                {
+                    acceleration.y -= 0.005f;
+                }
             }
         }
 
-
+        //rotate car
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward * turnpower);
+            transform.Rotate(Vector3.forward * turnSpeed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.forward * -turnpower);
+            transform.Rotate(Vector3.forward * -turnSpeed);
         }
 
-        gas();
+        Moving();
         Debug.Log(acceleration.y);
     }
 
-    void gas()
+    void Moving()
     {
+        //Function which checks player input and reduces speed if car has no acceleration .
+        //
         if (!Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.Space))
         {
             if(acceleration.y >=0)
@@ -84,9 +116,26 @@ public class movement2 : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        //if (acceleration.y)
-        acceleration.y -= 0.1f;
-        Debug.Log("hit");
+
+        //Function which checks how fast the car is going and depending on current speed, lower it when colliding with object
+        //
+        if (acceleration.y <= 0.1)
+        {
+            acceleration.y -= 0.03f;
+            Debug.Log("slow");
+
+        }
+        else if (acceleration.y >= 0.2f)
+        {
+            acceleration.y -= 0.1f;
+            Debug.Log("fast");
+        }
+        else
+        {
+            acceleration.y -= 0.06f;
+            Debug.Log("middle");
+
+        }
     }
 
 
